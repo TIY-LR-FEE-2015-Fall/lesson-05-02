a420697e0d3efd174cf4604be4c105d4ae609e33
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-17 11:09:12 -0600

    Submit contact

--- contacts.js
+++ contacts.js
@@ -5,8 +5,21 @@ $('.toggle-form').on('click', function() {
 });
 
 // Submit form saves a contact
-$('.contact-form').on('submit', function() {
-  console.log('Foo');
+$('.contact-form').on('submit', function(ev) {
+  // Stop page from ACTUALLY submitting
+  ev.preventDefault();
+
+  var name = $(`#contact-name`).val();
+  var phone = $(`#contact-phone`).val();
+
+  $.ajax({
+    url: `http://tiny-lr.herokuapp.com/collections/contacts-rt`,
+    method: `POST`,
+    dataType: `json`,
+    data: {name, phone},
+  }).then((response) => {
+    debugger;
+  });
 });
 
 // Show existing contacts
